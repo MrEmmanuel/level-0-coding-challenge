7a3cde67e9c0ace74c15729d3aed7ad111f3c60c
MrEmmanuel <sejesoo@gmail.com>
2021-06-22 18:49:34 +0200

    Removed unneceesary comments

--- Task3.java
+++ Task3.java
@@ -1,14 +1,8 @@
-//Task 0.3
-
-
-//This method implement a method that greet Tshepo
 public class Task3 {
 
     public static void main(String[] args){
         hello("Tshepo");
     }
-
-    //Method to greet a person using their name
     public static void hello(String name){
 
         System.out.println("Hello "+ name + "!");
