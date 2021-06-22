7412cacd703403aea624a48120799fe96cd64ab1
MrEmmanuel <sejesoo@gmail.com>
2021-06-22 19:07:45 +0200

    Indentation and removed unnecessary comments

--- Task7.java
+++ Task7.java
@@ -1,34 +1,23 @@
-//Task 0.7
-
 public class Task7 {
 
 	public static void main(String[] args) {
 		
-			System.out.println(celsiusToFahrenheit(32) + "°F");
-			
-			System.out.println(fahrenheitToCelsius(74) + "°C");
-
+		System.out.println(celsiusToFahrenheit(32) + "°F");
+		System.out.println(fahrenheitToCelsius(74) + "°C");
 	}
-	
-	//This method convert the temperature from celsius to fahrenheit 
 	public static int celsiusToFahrenheit(int cel) {
 		
 		int celsius = cel;
 		double fahrenheit = 0;
-		
 		fahrenheit = (celsius *(1.8)) + 32;
 		
-		return (int) (fahrenheit);
+	return (int) (fahrenheit);
 	}
-	
-	//This method convert the temperature from fahrenheit to celsius 
 	public static int fahrenheitToCelsius(int fah) {
 		
 		int fahrenheit = fah;
-		
 		double celsius = ((fahrenheit) - 32) * (0.55555555556);    
 		
-		return (int)celsius;
+	return (int)celsius;
 	}
-
 }
