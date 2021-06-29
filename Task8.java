3d64c3171c164925fb1345f66feadbfd033499cd
MrEmmanuel <sejesoo@gmail.com>
2021-06-29 23:41:17 +0200

    Update Task8.java

--- Task8.java
+++ Task8.java
@@ -1,3 +1,4 @@
+
 public class Task8 {
 
 	public static void main(String[] args) {
@@ -16,48 +17,56 @@ public class Task8 {
 		int hours;
 		int minutes;
 		int number = num;
+		
 		if(number < 0){
 			System.out.println("Invalid number");   
 		}else {
 			if(number < 60 ) {
 				hours = 0;
 				minutes = number;
-				
 			}else {
-			
 				hours = number / 60;         
 				minutes = number%60; 
 			}
-		
-			if(hours == 0 && minutes == 0) {
-				System.out.println(number + " is "+ hours+ " hours, "+ minutes + " minutes");
-			}else if(hours <= 1 && minutes <= 1 ) {
-				
-				if(	hours <= 1 && minutes == 0) {
-					System.out.println(number + " is "+ hours+ " hour, "+ minutes + " minutes ");
-				}else if(hours == 0 && minutes <= 1){
-					System.out.println(number + " is "+ hours+ " hour, "+ minutes + " minutes ");
+			if(hours <= 1) {	
+				if(minutes <= 1) {
+					checkLessThanMinute(minutes, hours, number);
 				}else {
-					System.out.println(number + " is "+ hours+ " hour, "+ minutes + " minute ");
-				}
-				
-			}else if(hours <= 1 && minutes > 1) {
-				if(	hours == 0) {
-					System.out.println(number + " is "+ hours+ " hours, "+ minutes + " minutes");
-				}else {
-					System.out.println(number + " is "+ hours+ " hour, "+ minutes + " minutes");
+					checkGreaterThanMinute(minutes, hours, number);
 				}
 			}else if(hours > 1 && minutes <= 1) {
-				
-				if(	minutes == 0) {
-					System.out.println(number + " is "+ hours+ " hours, "+ minutes + " minutes");
+				if(minutes == 0) {
+					zeroHourZeroMinute(minutes, hours, number);
 				}else {
 					System.out.println(number + " is "+ hours+ " hours, "+ minutes + " minute");
 				}
 			}else{
-				System.out.println(number + " is "+ hours+ " hours, "+ minutes + " minutes");
+				zeroHourZeroMinute(minutes, hours, number);
 			}
 		}
 	}
-
+	public static void checkLessThanMinute(int aMinute, int anHour, int num) {
+		int minutes = aMinute;
+		int hours = anHour;
+		int number = num;
+		
+		if((hours <= 1 && minutes == 0) || (hours == 0 && minutes <= 1)) {
+			System.out.println(number + " is "+ hours+ " hour, "+ minutes + " minutes ");
+		}else{
+			System.out.println(number + " is "+ hours+ " hour, "+ minutes + " minute ");
+		}
+	}
+	public static void zeroHourZeroMinute(int aMinute, int anHour, int num) {
+		System.out.println(num + " is "+ anHour+ " hours, "+ aMinute + " minutes");
+	} 
+	public static void checkGreaterThanMinute(int aMinute, int anHour, int num) {
+		int minutes = aMinute;
+		int hours = anHour;
+		int number = num;
+		if(	hours == 0) {
+			System.out.println(number + " is "+ hours+ " hours, "+ minutes + " minutes");
+		}else {
+			System.out.println(number + " is "+ hours+ " hour, "+ minutes + " minutes");
+		}	
+	}
 }
